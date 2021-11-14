d3c1fb8462f10e39a0841b29bd26fd04e821713d
adityaverm-a <aditya.se.403@gmail.com>
2021-11-14 21:45:21 +0530

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Layout from '@/components//layout'
+import Layout from '@/components/layout'
 import PorjectsCard from '@/components//PorjectsCard'
 import BlogPostCard from '@/components//BlogPostCard'
 
